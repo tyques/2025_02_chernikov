670227643cbf131e9e14489d3f6f768474ff41f0
Антон Черников <avch28@mail.ru>
2025-10-01 12:36:41 +0700

    2 Итерация проверки Исправил все недочеты.

--- task1/src/main/java/ru/shift/Main.java
+++ task1/src/main/java/ru/shift/Main.java
@@ -9,31 +9,29 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        try (Scanner scanner = new Scanner(System.in)) {
-            int tableSize;
+        Scanner scanner = new Scanner(System.in);
+        int tableSize;
 
-            while (true) {
-                System.out.print("Enter the table size (an integer from 1 to 32): ");
-                try {
-                    tableSize = scanner.nextInt();
+        while (true) {
+            System.out.print("Enter the table size (an integer from 1 to 32): ");
+            try {
+                tableSize = scanner.nextInt();
 
-                    if (tableSize >= Constants.MIN_TABLE_SIZE && tableSize <= Constants.MAX_TABLE_SIZE) {
-                        break;
-                    } else {
-                        System.err.println("Error: The table size must be in the range from 1 to 32. Please try again.");
-                    }
-                } catch (InputMismatchException e) {
-                    System.err.println("Error: The input must be an integer. Please try again.");
-                    scanner.next();
+                if (tableSize >= Constants.MIN_TABLE_SIZE && tableSize <= Constants.MAX_TABLE_SIZE) {
+                    break;
+                } else {
+                    System.err.println("Error: The table size must be in the range from 1 to 32. Please try again.");
                 }
+            } catch (InputMismatchException e) {
+                System.err.println("Error: The input must be an integer. Please try again.");
+                scanner.next();
             }
-
-            MultiplicationTableService multiplicationTableService = new MultiplicationTableService();
-            MultiplicationTable multiplicationTable = new MultiplicationTable(tableSize);
-
-            String table = multiplicationTableService.generateTableAsString(multiplicationTable);
-            System.out.println(table);
-
         }
+
+        MultiplicationTable multiplicationTable = new MultiplicationTable(tableSize);
+        MultiplicationTableService multiplicationTableService = new MultiplicationTableService(multiplicationTable);
+
+        String table = multiplicationTableService.generateTableAsString();
+        System.out.println(table);
     }
 }
--- task1/src/main/java/ru/shift/domain/services/MultiplicationTableService.java
+++ task1/src/main/java/ru/shift/domain/services/MultiplicationTableService.java
@@ -4,47 +4,57 @@ import ru.shift.domain.entity.Constants;
 import ru.shift.domain.entity.MultiplicationTable;
 
 public class MultiplicationTableService {
-    public String generateTableAsString(MultiplicationTable multiplicationTable) {
+    private final int tableSize;
+    private final int lengthOfFirstColumn;
+    private final int cellSize;
+
+    public MultiplicationTableService(MultiplicationTable multiplicationTable) {
+        this.tableSize = multiplicationTable.getTableSize();
+        this.lengthOfFirstColumn = multiplicationTable.getLengthOfFirstColumn();
+        this.cellSize = multiplicationTable.getCellSize();
+    }
+
+    public String generateTableAsString() {
         StringBuilder stringBuilder = new StringBuilder();
 
-        writeFirstRow(multiplicationTable, stringBuilder);
-        writeDividingRow(multiplicationTable, stringBuilder);
-        for (int row = 1; row <= multiplicationTable.getTableSize(); row++) {
+        writeFirstRow(stringBuilder);
+        writeDividingRow( stringBuilder);
+        for (int row = 1; row <= tableSize; row++) {
             stringBuilder.append(Constants.NEW_LINE);
-            writeFirstElementInRow(row, multiplicationTable.getLengthOfFirstColumn(), stringBuilder);
+            writeFirstElementInRow(row, lengthOfFirstColumn, stringBuilder);
 
-            for (int column = 1; column <= multiplicationTable.getTableSize(); ++column) {
+            for (int column = 1; column <= tableSize; ++column) {
                 stringBuilder.append(Constants.STICK);
-                stringBuilder.append(Constants.SPACE.repeat(multiplicationTable.getCellSize() - calcDigitsInCell(row, column)));
+                stringBuilder.repeat(Constants.SPACE, cellSize - calcDigitsInCell(row, column));
                 stringBuilder.append(row * column);
             }
-            writeDividingRow(multiplicationTable, stringBuilder);
+            writeDividingRow(stringBuilder);
         }
         return stringBuilder.toString();
     }
 
-    private void writeFirstRow(MultiplicationTable multiplicationTable, StringBuilder stringBuilder) {
-        stringBuilder.append(Constants.SPACE.repeat(multiplicationTable.getLengthOfFirstColumn()));
-        for (int i = 1; i <= multiplicationTable.getTableSize(); i++) {
+    private void writeFirstRow(StringBuilder stringBuilder) {
+        stringBuilder.repeat(Constants.SPACE, lengthOfFirstColumn);
+        for (int i = 1; i <= tableSize; i++) {
             stringBuilder.append(Constants.STICK);
-            stringBuilder.append(Constants.SPACE.repeat(multiplicationTable.getCellSize() - calcDigitsInCell(1, i)));
+            stringBuilder.repeat(Constants.SPACE, cellSize - calcDigitsInCell(1, i));
             stringBuilder.append(i);
         }
     }
 
-    private void writeDividingRow(MultiplicationTable multiplicationTable, StringBuilder stringBuilder) {
+    private void writeDividingRow(StringBuilder stringBuilder) {
         stringBuilder.append(Constants.NEW_LINE);
-        stringBuilder.append(Constants.MINUS.repeat(multiplicationTable.getLengthOfFirstColumn()));
+        stringBuilder.repeat(Constants.MINUS, lengthOfFirstColumn);
 
-        for (int j = 0; j < multiplicationTable.getTableSize(); j++) {
+        for (int j = 0; j < tableSize; j++) {
             stringBuilder.append(Constants.PLUS);
-            stringBuilder.append(Constants.MINUS.repeat(multiplicationTable.getCellSize()));
+            stringBuilder.repeat(Constants.MINUS, cellSize);
         }
     }
 
     private void writeFirstElementInRow(int row, int cellSize, StringBuilder stringBuilder) {
         int countOfSpaces = cellSize - calcDigitsInCell(row, 1);
-        stringBuilder.append(Constants.SPACE.repeat(countOfSpaces));
+        stringBuilder.repeat(Constants.SPACE, countOfSpaces);
         stringBuilder.append(row);
     }
 
