d2d7a8a474a1d5426e5d3090a705713f73e99489
Черников Антон <avch28@mail.ru>
2025-10-13 17:53:52 +0700

    Основу логики сделал

--- task2/src/main/java/lab/shift/data/Shape.java
+++ task2/src/main/java/lab/shift/data/Shape.java
@@ -1,7 +1,13 @@
 package lab.shift.data;
 
-public sealed interface Shape {
+public sealed interface Shape permits Circle, Rectangle, Triangle {
     double getArea();
+
     double getPerimeter();
+
     String getDetailedInfo();
+
+    default String getName() {
+        return this.getClass().getSimpleName();
+    }
 }
